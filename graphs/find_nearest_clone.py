d6b63418659c800df695412d86161d8e471bcdfa
Dylan Emery <dylane884@gmail.com>
2019-12-22 22:59:05 -0500

    Finished and spruced up this answer

--- graphs/find_nearest_clone.py
+++ graphs/find_nearest_clone.py
@@ -18,51 +18,53 @@ import sys
 #
 def findShortest(graph_nodes, graph_from, graph_to, ids, val):
     # Make an adjacency list
+    graph_as_adjacency_list = make_adjacency_list(graph_nodes, graph_from, graph_to)
+
+    # for each color of val, run dfs looking for another color val
+    current_best_path_length = graph_nodes + 5
+    for index, color in enumerate(ids):
+        index += 1
+        if val == color:
+            new_length = dfs(graph_as_adjacency_list, val, index, ids, current_best_path_length)
+            if new_length:
+                current_best_path_length = new_length
+    if current_best_path_length < graph_nodes + 5:
+        return current_best_path_length
+    else:
+        return -1
+                
+def get_path_length_from_discovered_by_dict(discovered_by_dict, start_node, end_node):
+    cur = end_node
+    path_length = 0
+    while cur != start_node and path_length < len(discovered_by_dict):
+        path_length += 1
+        cur = discovered_by_dict[cur]
+    return path_length
+
+def dfs(graph_as_adjacency_list, searching_for_val, start_index, ids, current_best_path_length):
+    to_search_stack = graph_as_adjacency_list[start_index]
+    discovered_by_dict = {i : start_index for i in graph_as_adjacency_list[start_index]}
+    searched = [start_index]
+    while(len(to_search_stack)>0):
+        cur = to_search_stack.pop()
+        if ids[cur-1] == searching_for_val:
+            distance = get_path_length_from_discovered_by_dict(discovered_by_dict, start_index, cur)
+            if distance < current_best_path_length:
+                current_best_path_length = distance
+                return current_best_path_length
+        searched.append(cur)
+        neighbors = graph_as_adjacency_list[cur]
+        for neighbor in neighbors:
+            if neighbor not in searched:
+                to_search_stack.append(neighbor)
+                discovered_by_dict[neighbor] = cur
+    return False                    
+def make_adjacency_list(graph_nodes, graph_from, graph_to):
     graph_as_adjacency_list = {node : [] for node in range(1, graph_nodes+1)}
     for g_from, g_to in zip(graph_from, graph_to):
         graph_as_adjacency_list[g_from].append(g_to)
         graph_as_adjacency_list[g_to].append(g_from)
-    
-    print(graph_as_adjacency_list)
-
-    # for each color of val, run dfs looking for another color val
-    solution_nodes = []
-    path_length = graph_nodes + 5
-    for index, color in enumerate(ids):
-        print(index, color)
-        index += 1
-        if val == color and index not in solution_nodes:
-            #dfs(index+1)
-            to_search_stack = graph_as_adjacency_list[index]
-            searched = [index]
-            distance = 0
-            while(len(to_search_stack)>0):
-                cur = to_search_stack.pop()
-                distance += 1
-                if ids[cur-1] == val:
-                    if distance < path_length:
-                        solution_nodes = [cur, index]
-                        path_length = distance
-                    break
-                searched.append(cur)
-                neighbors = graph_as_adjacency_list[cur]
-                for neighbor in neighbors:
-                    if neighbor not in searched:
-                        to_search_stack.append(neighbor)
-    if solution_nodes:
-        return path_length
-    else:
-        return -1
-                
-
-
-            
-
-    # take the min, or return 0
-
-def dfs(val):
-    pass
-
+    return graph_as_adjacency_list
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
