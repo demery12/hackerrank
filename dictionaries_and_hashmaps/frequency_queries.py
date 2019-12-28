69682d50fc761c49a73d3d840da97c9661585047
Dylan Emery <dylane884@gmail.com>
2019-12-28 00:18:48 -0500

    optimized last nights question

--- dictionaries_and_hashmaps/frequency_queries.py
+++ dictionaries_and_hashmaps/frequency_queries.py
@@ -8,21 +8,31 @@ import sys
 
 # Complete the freqQuery function below.
 def freqQuery(queries):
-    num_counts = {}
+    count_of_num = {}
+    count_of_frequency = {1:0}
     to_return = []
     for query in queries:
         operation = query[0]
         data = query[1]
         if operation == 1:
-            # built_list.append(data)
-            if data not in num_counts:
-                num_counts[data] = 0
-            num_counts[data] += 1
+            if data not in count_of_num or count_of_num[data]==0:
+                count_of_num[data] = 1
+                count_of_frequency[1] += 1
+            else:
+                count_of_frequency[count_of_num[data]] -= 1
+                count_of_num[data] += 1
+                if count_of_num[data] not in count_of_frequency:
+                    count_of_frequency[count_of_num[data]] = 0
+                count_of_frequency[count_of_num[data]] += 1
+                
         if operation == 2:
-            if data in num_counts and num_counts[data] > 0:
-                num_counts[data] -= 1
+            if data in count_of_num and count_of_num[data] > 0:
+                count_of_frequency[count_of_num[data]] -= 1
+                count_of_num[data] -= 1
+                if count_of_num[data] > 0:
+                    count_of_frequency[count_of_num[data]] += 1
         if operation == 3:
-            if data in num_counts.values():
+            if data in count_of_frequency and count_of_frequency[data]>0:
                 to_return.append(1)
             else:
                 to_return.append(0)
@@ -46,4 +56,3 @@ if __name__ == '__main__':
     fptr.write('\n')
 
     fptr.close()
-
